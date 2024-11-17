b3dd095ba36369728505d638a7cdd3b36889a76d
Peter Elmered <peter@elmered.com>
2024-11-17 17:09:06 +0100

    Add pagination support

--- app/Http/Controllers/ShowProductsController.php
+++ app/Http/Controllers/ShowProductsController.php
@@ -2,17 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use App\Actions\FetchAllProductDataAction;
 use App\Actions\FetchProductsAction;
-use App\DataObjects\ProductData;
 use App\Http\Resources\ProductCollectionResource;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ShowProductsController extends Controller
 {
     public function __invoke(Request $request, FetchProductsAction $fetchProductsAction)
     {
         $products = $fetchProductsAction->execute();
+
+        $totalCount = $products->count();
+        $pageSize = (int) ($request->get('page_size') ?? 5);
+        $currentPage = (int) ($request->get('page') ?? 1);
+
+        $products = $products->slice(($currentPage - 1) * $pageSize, $pageSize);
+
+        $products = new LengthAwarePaginator(
+            $products,
+            $totalCount,
+            $pageSize,
+            $currentPage,
+        );
 
         return new ProductCollectionResource($products);
     }
